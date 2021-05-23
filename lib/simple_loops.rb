3e197072dcf621d3c0cbe6b18ef40d2a96bc4de9
ambitious-caller-8426 <anhcdang@gmail.com>
2021-05-23 23:35:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
 # Write your methods here
+def loop_message_five_times(message)
+  count = 5
+  while count > 0 do
+    puts message
+    count -= 1
+end
+
+def loop_message_n_times
+  
+end
+
+def output_array
+  
+end
+
+def return_string_array
+  
+end
